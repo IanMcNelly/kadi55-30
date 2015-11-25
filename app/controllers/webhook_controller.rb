3962f967ee69ce8a4c1d56de7ac579962b28598b
IanMcNelly <ian.mcnelly@coupa.com>
2015-11-24 21:21:28 -0800

    Add webhook controller

---
+++ app/controllers/webhook_controller.rb
@@ -0,0 +1,10 @@
+class DescriptorController < ApplicationController
+  # TODO: Figure out better way to do this
+  skip_before_filter  :verify_authenticity_token
+
+  def parse
+  	puts "IN webhook parse!"
+  	
+  end
+
+end
