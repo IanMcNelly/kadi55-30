80dd6989901dfe1575818c1402866d186330fa6d
IanMcNelly <ian.mcnelly@coupa.com>
2015-11-24 17:41:06 -0800

    Fix descriptor

--- app/controllers/descriptor_controller.rb
+++ app/controllers/descriptor_controller.rb
@@ -6,10 +6,10 @@ class DescriptorController < ApplicationController
   	descriptor[:name] = "Kadi 55-30"
   	descriptor[:description] = "Integration for Hipchat to connect Destiny news and info"
   	descriptor[:key] = "com.test.kadi55-30"
-  	descriptor[:links][:homepage] = "https://mighty-meadow-7891.herokuapp.com/"
-  	descriptor[:links][:self] = "https://mighty-meadow-7891.herokuapp.com/descriptor/"
-  	descriptor[:capabilities][:hipchatApiConsumer][:fromName] = "Kadi 55-30"
-  	descriptor[:capabilities][:hipchatApiConsumer][:scopes] = ["send_notification"]
+  	descriptor[:links] = {:homepage => "https://mighty-meadow-7891.herokuapp.com/",
+  		:self => "https://mighty-meadow-7891.herokuapp.com/descriptor/"}
+  	
+  	descriptor[:capabilities] = {:hipchatApiConsumer => {:fromName => "Kadi 55-30", :scopes => ["send_notification"]}}
 
   	render :json => descriptor.to_json
   end
