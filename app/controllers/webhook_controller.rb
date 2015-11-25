8ca662258e574b8165a90416040e537e1069442a
IanMcNelly <ian.mcnelly@coupa.com>
2015-11-24 21:28:05 -0800

    Copy paste problem

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,10 +1,10 @@
-class DescriptorController < ApplicationController
+class WebhookController < ApplicationController
   # TODO: Figure out better way to do this
   skip_before_filter  :verify_authenticity_token
 
   def parse
   	puts "IN webhook parse!"
-  	
+
   end
 
 end
